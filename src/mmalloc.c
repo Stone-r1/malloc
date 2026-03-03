48aebfe9f5bf7a087ce6d424e26f72e3b8798bf8
stoney <sarkisyanmihail11@gmail.com>
2026-03-04 02:19:47 +0400

    Feat(malloc): finding free segments in free list

--- src/mmalloc.c
+++ src/mmalloc.c
@@ -1,32 +1,48 @@
 #include <stdio.h>
 #include <unistd.h> 
 
-// Let's say I have a chunk of 4KB.
-// If system requires more, request it from hardware.
-// Maintain free list
-
 // Node struct of the free list
 typedef struct __node_t {
-    size_t chunkSize;
+    size_t segmentSize;
     struct __node_t *next;
 } node_t;
 
 // Header of the allocated segment
 typedef struct __header_t {
-    size_t sizeOfSegment;
-    int magicIdentifier;
+    size_t segmentSize;
 } header_t;
 
 // Free list to maintain allocated segments
 static node_t *freeList = NULL;
 
 
+// ========= Free List ===============
 void freeList_init(size_t size, void *ptr) {
     freeList = (node_t *)ptr;
-    freeList->chunkSize = size - sizeof(node_t);
+    freeList->segmentSize = size - sizeof(node_t);
     freeList->next = NULL;
 }
 
+node_t *findSegment(size_t size) {
+    node_t *bestMatch = freeList;
+    node_t *cur = freeList;
+
+    while (cur != NULL) {
+        if (cur->segmentSize >= size && cur->segmentSize <= bestMatch->segmentSize) {
+            bestMatch = cur;
+        }
+        cur = cur->next;
+    }
+
+    return bestMatch->segmentSize < size ? NULL : bestMatch;
+}
+
+void manageFreeList(node_t *block, size_t size) {
+    return;
+}
+
+
+// ========= Malloc =============
 void *mmalloc_init(size_t size) {
     void *ptr = sbrk(size);
 
@@ -39,13 +55,27 @@ void *mmalloc_init(size_t size) {
     return ptr;
 }
 
+// Search for the free chunk of memory in free list
+// Take the smallest possible chunk
+// If none found return NULL
+// Take that memory + header size
+// Change the value from free list
+// return pointer to the memory (not header)
 void *mmalloc(size_t size) {
-    // Search for the free chunk of memory in free list
-    // Take the smallest possible chunk
-    // If none found return NONE
-    // Take that memory + header size
-    // Change the value from free list
-    // return pointer to the memory (not header)
+    size_t actualSize = size + sizeof(header_t);
+    node_t *block = findSegment(actualSize);
+
+
+    if (block == NULL) {
+        perror("Couldn't find the segment of the requested size");
+        return NULL;
+    }
+
+    manageFreeList(block, actualSize);
+    header_t *header = (header_t *)block;
+    header->segmentSize = size;
+
+    return (void *)((char *)header + sizeof(header_t));
 }
 
 void free(void *ptr) {
@@ -56,5 +86,8 @@ void free(void *ptr) {
 
 int main() {
     printf("Future Malloc!\n");
+
+    mmalloc_init(4096);
+    printf("Allocated %zu KB of memory!\n", freeList->segmentSize);
     return 0;
 }
